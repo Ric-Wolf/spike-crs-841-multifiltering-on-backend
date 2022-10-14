a470141643181d17f06a938dc39fbd63b689e3a4
Ricardo Rico <rrico@lwolf.com>
2022-10-14 10:24:21 -0500

    spike done w/ js color-state reset search

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Union
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import numpy as np
@@ -9,14 +9,14 @@ app = Flask(__name__)
 
 TXT_ALL_OPTION = "-- ALL --"
 
-
+# ? temporary to create the data frame, the most important thing is the name of the columns
 def get_filter_dropdown_values():
     row1 = ["Ric", ("Spanish", "English", "French"), "Colombia"]
     row2 = ["Alejo", ("Spanish", "English"), "Colombia"]
     row3 = ["Gary", "English", "US"]
     row4 = ["Luis", ("Spanish", "English"), "Panama"]
     row5 = ["Orlando", ("English", "Philippino"), "The Philippines"]
-    row6 = ["Chris", "English", "US"]
+    row6 = ["Chris", "English", ("US")]
 
     rows = [row1, row2, row3, row4, row5, row6]
     data = []
@@ -35,22 +35,38 @@ def get_filter_dropdown_values():
     df = pd.DataFrame(data=data, columns=["member", "language", "country"])
 
     column_data = df.to_dict(orient="list")
-    new_column_data = []
+    new_column_data = {}
     for key, val in column_data.items():
         val = list(set(val))
         val.sort()
-        new_column_data.append([key, val])
+        new_column_data[key] = val
 
     return df, new_column_data
 
 
 class Filter:
-    def __init__(self):
-        self.filter = {}
+    def __init__(self, column_dict: Dict):
         self.filtering = False
+        self.filter = {}
+        self.last = {}
+        self.is_filter_status = {}
+        self._initial = column_dict
+        self.initialize()
+
+    def initialize(self):
+        for key, val in self._initial.items():
+            self.filter[key] = None
+            self.last[key] = val
+            self.is_filter_status[key] = 0
 
     def get_filter(self):
         return self.filter
+
+    def get_last(self):
+        return self.last
+
+    def get_status(self):
+        return self.is_filter_status
 
     def is_filter_active(self):
         return self.filtering
@@ -59,93 +75,117 @@ class Filter:
         self.filtering = is_filter_active
 
     def reset(self):
+        self.filtering = False
         self.filter = {}
+        self.last = {}
+        self.is_filter_status = {}
+        self.initialize()
 
-    def update(self, fil_val_dict: Dict):
-        for key, val in fil_val_dict.items():
-            self.filter[key] = val
+    def update_filter(self, key: str, val: Union[str, None]):
+        self.filter[key] = val
+        if val is not None:
+            self.filtering = True
 
+    def update_last(self, columns_dict: Dict):
+        for key, value in columns_dict.items():
+            self.last[key] = value
 
-filter_data = Filter()
+    def update_status(self, key: str, val: Union[str, None]):
+        self.is_filter_status[key] = val
 
 
 @app.route("/_dp")
 def update_dropdown():
 
     df, columns = get_filter_dropdown_values()
-    output_txt = ""
-    is_filtered_columns = {}
-
-    to_filter = filter_data.get_filter()
-
-    for col in columns:
-        if col[0] not in to_filter.keys():
-            filter_data.update({col[0]: None})
-        is_filtered_columns[col[0]] = False
+    output_txt = {"member": "None", "language": "None", "country": "None"}
 
     call = request.args.get("call", type=str)
 
     selected_value = request.args.get(str(call), type=str)
 
     if call == "reset":
-        filter_data.set_filtering(False)
-        output_txt = "Here we display some output based on the selection"
         filter_data.reset()
 
     if selected_value is not None:
-        if selected_value == TXT_ALL_OPTION:
-            selected_value = None
-        filter_data.update({call: selected_value})
+        selected_value = selected_value if (selected_value != TXT_ALL_OPTION) else None
+        filter_data.update_filter(call, selected_value)
 
-    print(is_filtered_columns)
+    updated_columns = filter_data.get_last()
 
-    # selected_lang = request.args.get_filter("lang", type=str)
-    # selected_country = request.args.get_filter("country", type=str)
-
-    updated_columns = columns
     if call != "reset":
-        to_filter = filter_data.get_filter()
-        output_txt = str(to_filter)
-        for key, value in to_filter.items():
+
+        for key, value in filter_data.get_filter().items():
             if value is not None:
                 df = df.loc[df[key] == value]
-                filter_data.set_filtering(True)
-        for col in updated_columns:
-            col[1] = list(set(df[col[0]]))
+
+        for key in updated_columns.keys():
+            updated_columns[key] = list(set(df[key]))
 
     if filter_data.is_filter_active():
-        to_filter = filter_data.get_filter()
-        for key, val in to_filter.items():
+        for key, val in filter_data.get_filter().items():
             if val is None:
-                is_filtered_columns[key] = True
+                filter_data.update_status(key, 1)
+            else:
+                filter_data.update_status(key, -1)
 
     if call == "print":
-        output_txt = str(updated_columns)
+        for key, item in filter_data.get_status().items():
+            if item == -1:
+                output_txt[key] = filter_data.get_filter()[key]
 
-    for col in updated_columns:
-        if len(col[1]) > 1:
-            col[1].sort()
-            col[1].insert(0, TXT_ALL_OPTION)
+    dp_data_list = {}
+    for key, col in updated_columns.items():
+        if len(col) > 1 and isinstance(col, list):
+            col.sort()
+            col.insert(0, TXT_ALL_OPTION)
 
-    dp_data_list = []
-    for col in updated_columns:
+        filter_element = filter_data.get_filter()[key]
+        if filter_element is not None:
+            temp_elem = []
+            for item in filter_data.get_last()[key]:
+                if item != filter_element:
+                    temp_elem.append(item)
+                    continue
+            temp_elem.sort()
+            col.extend(temp_elem)
+            col.insert(1, TXT_ALL_OPTION)
+
         dp_data_html = ""
-        for entry in col[1]:
+        for entry in col:
             dp_data_html += '<option value="{}">{}</option>'.format(entry, entry)
-        dp_data_list.append(dp_data_html)
-    return jsonify(dp_data=dp_data_list, print_txt=output_txt)
+        dp_data_list[key] = dp_data_html
+        filter_element = filter_data.get_filter()[key]
+
+        if col[0] == TXT_ALL_OPTION and col[1:] == columns[key]:
+            filter_data.update_status(key, 0)
+
+        if TXT_ALL_OPTION in col:
+            col.remove(TXT_ALL_OPTION)
+
+    filter_data.update_last(updated_columns)
+    print(50 * "*")
+    # print(filter_data.get_filter().values())
+    # print(filter_data.get_status().values())
+    # print(output_txt)
+    print(
+        f"call: {call}\ndpData {updated_columns}\nstate: {filter_data.get_status()}\nallConst: {TXT_ALL_OPTION}"
+    )
+    print(50 * "*")
+    return jsonify(
+        dpData=dp_data_list,
+        printFilter=output_txt,
+        state=filter_data.get_status(),
+        allConst=TXT_ALL_OPTION,
+    )
 
 
 @app.route("/")
 def index():
-
-    """
-    Initialize the dropdown menues
-    """
-
     all_columns = get_filter_dropdown_values()[1]
-    for col in all_columns:
-        col[1].insert(0, TXT_ALL_OPTION)
+    for col in all_columns.values():
+        col.insert(0, TXT_ALL_OPTION)
+    filter_data.initialize()
     return render_template(
         "index.html",
         all_columns=all_columns,
@@ -153,4 +193,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3000)
+    filter_data = Filter(get_filter_dropdown_values()[1])
+    app.run(debug=True, host="localhost", port=3000)
--- static/js/script.js
+++ static/js/script.js
@@ -1,80 +1,91 @@
 $(document).ready(function () {
   // dropdown box
   $("#dp0").change(function () {
-    $.getJSON("/_dp", {
-      call: "member",
-      member: $("#dp0").val(),
-      lang: $("#dp1").val(),
-      country: $("#dp2").val(),
-    }).success(function (data) {
-      // $('#dp0').html(data.dp_data[0]),
-      $("#dp1").html(data.dp_data[1]),
-        $("#dp2").html(data.dp_data[2]),
-        $("#dp1_lab").css("color", "red"),
-        $("#dp2_lab").css("color", "red");
+    $.getJSON("/_dp", getDictForPython("member")).success(function (result) {
+      updateDropdowns(result), applyColorDropdowns(result);
     });
   });
   //end dp
 
   //dropdown box
   $("#dp1").change(function () {
-    $.getJSON("/_dp", {
-      call: "language",
-      member: $("#dp0").val(),
-      language: $("#dp1").val(),
-      country: $("#dp2").val(),
-    }).success(function (data) {
-      $("#dp0").html(data.dp_data[0]),
-        // $('#dp1').html(data.dp_data[1]),
-        $("#dp2").html(data.dp_data[2]),
-        $("#dp0_lab").css("color", "red"),
-        $("#dp2_lab").css("color", "red");
+    $.getJSON("/_dp", getDictForPython("language")).success(function (result) {
+      updateDropdowns(result), applyColorDropdowns(result);
     });
   });
   //end dp
 
   //dropdown box
   $("#dp2").change(function () {
-    $.getJSON("/_dp", {
-      call: "country",
-      member: $("#dp0").val(),
-      lang: $("#dp1").val(),
-      country: $("#dp2").val(),
-    }).success(function (data) {
-      $("#dp0").html(data.dp_data[0]),
-        $("#dp1").html(data.dp_data[1]),
-        //$("#dp2").html(data.dp_data[2]),
-        $("#dp0_lab").css("color", "red"),
-        $("#dp1_lab").css("color", "red");
+    $.getJSON("/_dp", getDictForPython("country")).success(function (result) {
+      updateDropdowns(result), applyColorDropdowns(result);
     });
   });
   //end dp
 
   //reset button
   $("#reset_btn").click(function () {
-    $.getJSON("_dp", {
-      call: "reset",
-    }).success(function (data) {
-      $("#dp0").html(data.dp_data[0]),
-        $("#dp1").html(data.dp_data[1]),
-        $("#dp2").html(data.dp_data[2]),
-        $("#dp0_lab").css("color", "black"),
-        $("#dp1_lab").css("color", "black"),
-        $("#dp2_lab").css("color", "black"),
-        $("#print_txt").text(
-          "Here we display some output based on the selection"
-        );
+    $.getJSON("/_dp", getDictForPython("reset")).success(function (result) {
+      updateDropdowns(result), applyColorDropdowns(result), updateTXT(result);
     });
   });
   //end
 
-  // implementar no colorear lo que activo un filtro
-
   //print button
   $("#print_btn").on("click", function () {
-    $.getJSON("/_dp", { call: "print" }).success(function (result) {
-      $("#print_txt").text(result.print_txt);
+    $.getJSON("/_dp", getDictForPython("print")).success(function (result) {
+      updateTXT(result);
     });
   });
   //end
 });
+
+function getDictForPython(dpName) {
+  return {
+    call: dpName,
+    member: $("#dp0").val(),
+    language: $("#dp1").val(),
+    country: $("#dp2").val(),
+  };
+}
+
+function updateTXT(result) {
+  $("#print_members").text(result.printFilter["member"]),
+    $("#print_languages").text(result.printFilter["language"]),
+    $("#print_countries").text(result.printFilter["country"]);
+}
+
+function updateDropdowns(result) {
+  $("#dp0").html(result.dpData["member"]),
+    $("#dp1").html(result.dpData["language"]),
+    $("#dp2").html(result.dpData["country"]);
+}
+
+const filteredMany = "IndianRed";
+const filteredOne = "grey";
+const filter = "#DAF7A6";
+const noFilter = "white";
+
+function applyBackgroundColor(elementID, colorConst) {
+  $(elementID).css({ "background-color": colorConst });
+}
+
+function applyColorByState(state, elementID, allConstant) {
+  if (state == 1) {
+    if ($(elementID).val() == allConstant) {
+      applyBackgroundColor(elementID, filteredMany);
+    } else {
+      applyBackgroundColor(elementID, filteredOne);
+    }
+  } else if (state == -1) {
+    applyBackgroundColor(elementID, filter);
+  } else {
+    applyBackgroundColor(elementID, noFilter);
+  }
+}
+
+function applyColorDropdowns(result) {
+  applyColorByState(result.state["member"], "#dp0", result.allConst),
+    applyColorByState(result.state["language"], "#dp1", result.allConst),
+    applyColorByState(result.state["country"], "#dp2", result.allConst);
+}
